ed5a726f64a1c818461c554ba5e5e27d54cdd00f
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2019-10-29 17:08:18 +0800

    Updates

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 function test() {
-  const element = document.getElementById("test-content");
+  const element = window.parent.document.getElementById("test-content");
   console.log(element);
   element.style.animation = "testFadeIn 300ms ease-out forwards";
 }
